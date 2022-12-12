3c7a70f1e516cecdd3d076255145a52cfa33eaef
HickeyAaron <115097595+HickeyAaron@users.noreply.github.com>
2022-12-12 16:48:01 +0000

    Update melanophore_plot.r

--- figure 2/melanophore_plot.r
+++ figure 2/melanophore_plot.r
@@ -1,39 +1,61 @@
 library(ggplot2)
 
 # create a variable which is the raw data - see csv file in figure 2 directory "Melanophore_coverage.csv"
-melano_numbers <- read.csv("Melanophore_coverage.csv", header = TRUE, sep = ",")
+melano_numbers <- read.csv("melano_coverage.csv", header = TRUE, sep = ",")
 
-#create ggplot using melano_numbers variable 
+#Create variable for figure colours
+cols <- c("#6896D4", "goldenrod2")
+
+
+#ggplot using melano_numbers variable 
 
 melano_plot <- ggplot(data = melano_numbers, 
-#Use Aesthetic mapping (aes) to assign variables to the x and y axis
+                      #Use Aesthetic mapping (aes) to assign variables to the x and y axis
                       aes(x=dpf, y=Melanophore_numbers, 
                           colour = "black", 
                           fill = "black"))+
   geom_point(alpha=1, size =1.5, colour = "black")+
-#Add rectangle to the plot which represents the range of dpf iridophores appear in the dataset 
-  geom_rect(aes(xmin=18, xmax=21, ymin=Inf, ymax=-Inf), 
-            fill="steelblue2", 
+  
+  #See additional csv file entitled "chromatophore_appearance_data.csv" for stats info included in plot
+  
+  #Add rectangle to the plot which represents the range of dpf iridophores appear in the dataset for shallow morphs
+  geom_rect(aes(xmin=20, xmax=23, ymin=Inf, ymax=-Inf), 
+            fill="goldenrod2", 
             alpha=0.01, 
             inherit.aes = FALSE) +
-#Add a line to the plot representing the mean of when iridophores appear in the dataset
-  geom_vline(xintercept = 20.17, 
-             color = "#999999", 
+  #Add rectangle to the plot which represents the range of dpf iridophores appear in the dataset for deep morphs
+  geom_rect(aes(xmin=18, xmax=21, ymin=Inf, ymax=-Inf), 
+            fill="#6896d4", 
+            alpha=0.01, 
+            inherit.aes = FALSE) +
+
+    #Add a line to the plot representing the mean iridophores appearance dpf in the dataset in the shallow morphs
+  geom_vline(xintercept = 21.4, 
+             color = "yellow", 
              alpha = 0.5, 
              size = 1)+
-#Add a linear model polynomial trendline to the melanophore datapoints
-  geom_smooth(method = lm, formula = y ~ poly(x, 3), se = FALSE, colour="black", size = 2)+
+  #Add a line to the plot representing the mean iridophores appearance dpf in the dataset in the deep morphs
+    geom_vline(xintercept = 19.14, 
+             color = "steelblue2", 
+             alpha = 0.5, 
+             size = 1)+
+  
+  #Add a linear model polynomial trendline to the melanophore datapoints
+  geom_smooth(aes(x = dpf, y = Melanophore_numbers, color = Morph), method = lm, formula = y ~ poly(x, 2), se = FALSE, size = 2)+
+  scale_color_manual(values = cols)+
   labs(title = " ", x ="dpf", y= "Melanophores (n)")+
-#Customize graph to remove standard R plot details
-  theme (legend.position="none", 
-         legend.title = element_blank(), 
-         plot.title = element_text(hjust = 0.5, size=12), 
+ 
+   #Customize graph to remove standard R plot details
+  theme(legend.position="none",
+         legend.title = element_blank(),
+         plot.title = element_text(hjust = 0.5, size=12),
          axis.text = element_text(size = 12, colour="black"), 
          panel.border = element_blank(), 
          panel.grid.major = element_blank(), 
-         panel.grid.minor = element_blank(), 
-         axis.line = element_line(colour = "black", size=1), 
+         panel.grid.minor = element_blank(),
+         axis.line = element_line(colour = "black", size=1),
          panel.background = element_rect(fill = "white"), 
-         axis.title.x = element_text(size=12, margin = margin(t = 10)))
+         axis.title.x = element_text(size=12, margin = margin(t = 10)))               
+                       
 #Diaplay the plot
 melano_plot

ee70daee747d6a1a311819d0acf08e68a0f0824a
HickeyAaron <115097595+HickeyAaron@users.noreply.github.com>
2023-01-05 16:44:54 +0000

    Update Xantho_Area_stats.r

--- Figure 4/Xantho_Area_stats.r
+++ Figure 4/Xantho_Area_stats.r
@@ -89,25 +89,34 @@ barplot(Posthoc_Male)
 cols <- c("#8D8A91", "#BF675E")
 
 
+#Define colours for the plots
+cols <- c("#8D8A91", "#BF675E")
+
+#theme
+
+theme <- theme (legend.title = element_blank(),
+                legend.position = c(.9,.1),
+                legend.background = element_rect(colour = "white", fill = "white"), 
+                legend.box.background = element_rect(colour = "white", fill = "white"), 
+                legend.key = element_rect(colour = "white", fill = "white"),
+                legend.text = element_text(size = 12, colour = "black"), 
+                plot.title = element_text(hjust = 0.25, size = 12), 
+                axis.text = element_text(size = 12, colour = "black"), 
+                panel.border = element_blank(), panel.grid.major = element_blank(), 
+                panel.grid.minor = element_blank(), 
+                axis.line = element_line(colour = "black", size=1),
+                panel.background = element_rect(fill = "white"),
+                plot.background = element_rect(colour = "white", fill = "white"), 
+                axis.title.y = element_text(size = 12, colour = "black", margin = margin (t = 12)), 
+                axis.title.x = element_text(size = 12, colour = "black", margin = margin(t = 10)))
+#plot 
+
 xantho_plot <- ggplot(data = xantho_scatter, aes(x = SL, y = Log_Area))+
-  geom_point(aes(x = SL, y = Log_Area, color = Sex, fill = Sex), alpha = 1, size = 1.5)+
-  scale_color_manual(values = cols)+
+  geom_point(aes(x = SL, y = Log_Area, color = Sex), alpha = 1, size = 1.5)+
+  scale_color_manual(labels = c("female", "male"),values = cols)+
   geom_smooth(aes(x = SL, y = Log_Area, color = Sex), method = lm, se = FALSE, size = 2)+
   labs(title = " ", x = "SL", y = bquote(Log_Area (mm^2)))+
-  theme (legend.title = element_blank(), 
-         legend.background = element_rect(colour = "white", fill = "white"), 
-         legend.box.background = element_rect(colour = "white", fill = "white"), 
-         legend.key = element_rect(colour = "white", fill = "white"),
-         legend.text = element_text(size = 12, colour = "black"), 
-         plot.title = element_text(hjust = 0.25, size = 12), 
-         axis.text = element_text(size = 12, colour = "black"), 
-         panel.border = element_blank(), panel.grid.major = element_blank(), 
-         panel.grid.minor = element_blank(), 
-         axis.line = element_line(colour = "black", size=1),
-         panel.background = element_rect(fill = "white"),
-         plot.background = element_rect(colour = "white", fill = "white"), 
-         axis.title.y = element_text(size = 12, colour = "black", margin = margin (t = 12)), 
-         axis.title.x = element_text(size = 12, colour = "black", margin = margin(t = 10)))
-
-
+  theme 
+  
+#Display plot
 xantho_plot
